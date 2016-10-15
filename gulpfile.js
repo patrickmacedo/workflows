3761895a523d71c5daa2f142d86e05774722923c
patrick macedo <patrick macedo>
2016-10-15 16:03:18 -0300

    Adicionado uglify e gulp-if

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
 var compass = require('gulp-compass');
 var connect = require('gulp-connect');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
 
 var env,
     coffeeSources,
@@ -48,6 +50,7 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest(outputDir + '/js'))
     .pipe(connect.reload())
 });
