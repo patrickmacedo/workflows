68e8cfabe39d7ef4458954f40215d84e6dc2cd5e
patrick macedo <patrick macedo>
2016-10-15 11:26:18 -0300

    Gulp Browserify adicionado, além de jquery e mustache

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -22,5 +23,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('scripts.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 })
