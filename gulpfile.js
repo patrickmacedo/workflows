b766df0146140269b92f36115ebc2cc2d4f26235
patrick macedo <patrick macedo>
2016-10-15 11:37:57 -0300

    Sass compilado

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -11,6 +12,7 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js',
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -18,11 +20,21 @@ gulp.task('coffee', function() {
       .on('error', gutil.log)
     )
     .pipe(gulp.dest('components/scripts'))
-})
+});
 
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('scripts.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
-})
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .pipe(gulp.dest('builds/development/css'))
+});
